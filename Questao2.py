3c9b6634472c9c7c6df4668b41080d81a6f98752
JoaoMartins18 <120115422+JoaoMartins18@users.noreply.github.com>
2022-12-09 03:08:26 +0000

    Atualização

--- Questao2.py
+++ Questao2.py
@@ -1,1 +1,23 @@
-print("Olá")
+#Leia a hora inicial, minuto inicial, hora final e minuto final de um jogo. A seguir calcule a duração do jogo.
+
+h_inicial = int(input("Qual a hora inicial? "))
+m_inicial = int(input("Qual o minuto inicial? "))
+h_final = int(input("Qual a hora final? "))
+m_final = int(input("Qual o minuto final? "))
+
+#Convertendo tudo para minutos
+m_final = m_final + (h_final*60)
+m_inicial = m_inicial + (h_inicial*60)
+
+#Tempo total do jogo em minutos
+time = m_final - m_inicial
+
+#Condição caso o tempo total em minutos dê negativo ou igual a zero.
+if(time <= 0):
+    time = time + (24*60)
+
+#Convertendo tudo para horas e o resto em minutos.
+hora = time // 60
+minuto = time % 60 
+
+print(f"O JOGO DUROU {hora} HORA(S) E {minuto} MINUTO(S).")
